aae0bdcd7df0f68e761a5f83db2ef46b49e25fb1
PRAVEEN J <praveen.j@microfocus.com>
2019-12-02 00:11:49 +0530

    Added Employee java class and Modified Test java class

---
+++ src/main/java/com/spring/module/Employee.java
@@ -0,0 +1,15 @@
+package com.spring.module;
+
+public class Employee {
+
+	
+	//employee class has been added into the module package
+	
+	public Employee() {
+	System.out.println("Defacult constructor for the Employee class....");
+	}
+	
+	public void getName(){
+		System.out.println("This is the employee Name method...");
+	}
+}
--- src/main/java/com/spring/module/Test.java
+++ src/main/java/com/spring/module/Test.java
@@ -2,8 +2,16 @@ package com.spring.module;
 
 public class Test {
 	
+	
+	public Test() {
+	
+		System.out.println("This is the test class default constructor....");
+	
+	}
 	public void printMethod(){
 		System.out.println("this is the test method....");
 	}
 
+	
+	
 }
