aedefeeff11bd4ce3fc28fcb7eb9211231dc54c8
Praveen J <27469713+praveenjayachandrappa@users.noreply.github.com>
2019-12-02 00:30:03 +0530

    Department.java
    This is the department class file added into the module package.

---
+++ src/main/java/com/spring/module/Department.java
@@ -0,0 +1,11 @@
+public class Department{
+
+      Department(){
+      System.out.println("This is the defacult constructor of the department class");
+      }
+
+  public static String getDeptNames(){
+      return "Computer science dept";
+  }
+
+}
