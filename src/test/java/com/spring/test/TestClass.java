9646cea3e177536e9a8031f995fdb1d5073b503c
Praveen J <27469713+praveenjayachandrappa@users.noreply.github.com>
2020-01-06 12:46:43 +0530

    Added code for getTestMethod()
    I have Added getTestMethod() in the Test Class.
    
    This is the new method added into the Test Class for getting objects.

--- src/test/java/com/spring/test/TestClass.java
+++ src/test/java/com/spring/test/TestClass.java
@@ -5,4 +5,9 @@ public class TestClass {
 	public static void TestMethod(){
 		System.out.println("This is the test Method....");
 	}
+
+	public static void getTest(){
+		System.out.println("This is the getTest Method in the Test class");
+		
+	}
 }
