d98f3b1c478aff373023e2046e1b27815cc6a05e
Praveen J <27469713+praveenjayachandrappa@users.noreply.github.com>
2020-01-06 13:02:15 +0530

    Added setMethod
    I have added test method decription

--- src/test/java/com/spring/test/TestClass.java
+++ src/test/java/com/spring/test/TestClass.java
@@ -5,4 +5,8 @@ public class TestClass {
 	public static void TestMethod(){
 		System.out.println("This is the test Method....");
 	}
+	
+	public static void setMethod(){
+		System.out.println("This is the set method in the test class");
+	}
 }
